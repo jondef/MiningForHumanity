65cc4399e447eb9000852b8b9ba0d3372258744d
jon <jdefilla@gmail.com>
2020-04-09 18:57:45 +0200

    basic functionality with gui and parameters

--- DonationMining/src/MainWindow.cpp
+++ DonationMining/src/MainWindow.cpp
@@ -4,8 +4,11 @@
 
 #include "MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::uiMainWindow) {
+MainWindow::MainWindow(int argc, char *argv[], QWidget *parent) : QMainWindow(parent), ui(new Ui::uiMainWindow) {
 	ui->setupUi(this);
+	this->argc = argc;
+	this->argv = argv;
+	qDebug() << QThread::idealThreadCount();
 //	QCoreApplication::setOrganizationName("MySoft"); MiningForHumanity
 //	QCoreApplication::setOrganizationDomain("mysoft.com");
 //	QCoreApplication::setApplicationName("Star Runner");
@@ -15,13 +18,33 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::uiMain
 		settings->setValue("editor/wrapMargin", checked);
 	});
 
-	connect(ui->buttonBox, &QDialogButtonBox::accepted, this, [&]() {
-		// save / connect to pools
+	connect(myProcess, &QProcess::readyReadStandardOutput, [this](){ ui->plainTextEdit->appendPlainText(myProcess->readAllStandardOutput()); });
+	connect(myProcess, &QProcess::readyReadStandardError, [this](){ ui->plainTextEdit->appendPlainText(myProcess->readAllStandardError()); });
+	connect(myProcess, qOverload<int, QProcess::ExitStatus>(&QProcess::finished), this, &MainWindow::close);
+	connect(ui->lineEdit_poolAdress, &QLineEdit::returnPressed, [this](){ myProcess->write(ui->lineEdit_poolAdress->text().toLatin1() + "\n"); ui->lineEdit_poolAdress->clear(); });
+
+	connect(ui->buttonBox, &QDialogButtonBox::accepted, this, [this]() -> void {
+		QString poolId = ui->lineEdit_poolAdress->text();
+		QString username = ui->lineEdit_username->text();
+		QString password = ui->lineEdit_password->text();
+		QString RIGID = "What";
+		bool SSLSupport = ui->checkBox_SSLSupport->isChecked();
+
+		QString program = "../../xmr-stak/bin/xmr-stak-rx";
+		QStringList arguments;
+		arguments << "--noTest" << "--noDevSupport" << "--currency" << "monero" << "--h-print-time" << "5" << "--url" << poolId << "--rigid" << RIGID << "--httpd" << "0" << "--user" << username << "--pass" << password;
+
+		myProcess->start(program, arguments);
+
+//		 save / connect to pools
 		// minimise window
 	});
-	connect(ui->buttonBox, &QDialogButtonBox::rejected, this, [&]() { this->close(); });
+	connect(ui->buttonBox, &QDialogButtonBox::rejected, this, [&]() {
+		this->close();
+	});
 }
 
 MainWindow::~MainWindow() {
 
 }
+
--- DonationMining/src/MainWindow.h
+++ DonationMining/src/MainWindow.h
@@ -8,57 +8,28 @@
 #include <QMainWindow>
 #include <QtCore>
 #include <QObject>
+#include <QString>
 #include <QSettings>
 #include "ui_MainWindow.h"
 
-#include "xmrstak/backend/backendConnector.hpp"
-#include "xmrstak/backend/globalStates.hpp"
-#include "xmrstak/backend/miner_work.hpp"
-#include "xmrstak/jconf.hpp"
-#include "xmrstak/misc/configEditor.hpp"
-#include "xmrstak/misc/console.hpp"
-#include "xmrstak/misc/executor.hpp"
-#include "xmrstak/misc/utility.hpp"
-#include "xmrstak/params.hpp"
-#include "xmrstak/version.hpp"
-
-#ifndef CONF_NO_HTTPD
-
-#include "xmrstak/http/httpd.hpp"
-
-#endif
-
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
-#include <time.h>
-
-#ifndef CONF_NO_TLS
-
-#include <openssl/err.h>
-#include <openssl/ssl.h>
-
-#endif
-
-#ifdef _WIN32
-#define strcasecmp _stricmp
-#include "xmrstak/misc/uac.hpp"
-#include <windows.h>
-#endif // _WIN32
+#include <future>
+#include <memory>
 
 
 class MainWindow : public QMainWindow {
 	Q_OBJECT
 
 public:
-	explicit MainWindow(QWidget *parent = nullptr);
+	explicit MainWindow(int argc, char *argv[], QWidget *parent = nullptr);
 
 	~MainWindow();
 
 private:
+	int argc;
+	char **argv;
 	Ui::uiMainWindow *ui;
 	QSettings *settings = new QSettings("MySoft", "Star Runner");
+	QProcess *myProcess = new QProcess(this);
 };
 
 
